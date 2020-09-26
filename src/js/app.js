01ac351b338dc4c5e6d4cbb4d6e71e2a835891d9
Evgeniy-27 <Li.Evgen@yandex.ru>
2020-09-26 18:56:52 +0700

    add all

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+/* eslint-disable consistent-return */
+/* eslint-disable array-callback-return */
+/* eslint-disable import/no-cycle */
+/* eslint-disable prefer-destructuring */
+// eslint-disable-next-line no-console
+console.log('it works!');
